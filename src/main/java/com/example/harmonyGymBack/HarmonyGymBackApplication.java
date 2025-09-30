14195ea987818ce4c9948e38f29b8bc37d65655b
roygro <princess18gro@gmail.com>
2025-09-30 13:31:58 -0600

    Initial commit: Spring Boot Gym Backend

---
+++ src/main/java/com/example/harmonyGymBack/HarmonyGymBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.harmonyGymBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmonyGymBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarmonyGymBackApplication.class, args);
+	}
+
+}
